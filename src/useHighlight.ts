3d24f40c0f785d12ac0dbb4e7ae8f373c3605ca0
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2022-12-29 16:35:40 +0100

    refactor using id instead of index as sot

--- src/useHighlight.ts
+++ src/useHighlight.ts
@@ -1,4 +1,4 @@
-import { ref, Ref, onMounted, isRef, computed, watch, nextTick } from 'vue';
+import { ref, Ref, onMounted, isRef, computed, unref, watch, nextTick, watchPostEffect } from 'vue';
 import { UseHighlightOptions } from './types';
 import { useResize, useScroll } from './internalComposables';
 
@@ -6,10 +6,9 @@ type Dataset = Record<string, string>;
 
 /**
  * This is a fixed value of 20px used when jumpToTop is false,
- * it prevents that the first section is marked as inactive
+ * it prevents the first section to be marked as inactive
  * maybe "too soon".
  */
-
 const BACK_TO_TOP_OFFSET = 20;
 
 export function getDataset(dataset: DOMStringMap): Dataset {
@@ -31,7 +30,7 @@ function getRects(
 	comparator?: '+' | '-',
 	userOffset: number = 0
 ) {
-	const map = new Map<number, number>();
+	const map = new Map<string, number>();
 	for (let i = 0; i < elements.length; i++) {
 		const rectProp = elements[i].getBoundingClientRect()[prop];
 		const condition =
@@ -39,28 +38,30 @@ function getRects(
 				? rectProp >= userOffset
 				: comparator === '-'
 				? rectProp <= userOffset
-				: true;
+				: true; // Get both positive and negative
 		if (condition) {
-			map.set(i, elements[i].getBoundingClientRect()[prop]);
+			map.set(elements[i].id, elements[i].getBoundingClientRect()[prop]);
 		}
 	}
 	return map;
 }
 
 /**
- * This function gets 'in advance' all section indices that would be
+ * This function gets 'in advance' all target ids that would be
  * excluded from the highlight process at the bottom of the page.
+ *
+ * Used onResize and whenever the user array changes.
  */
-function _setUnreachables(target: Ref<number[]>, elements: HTMLElement[]) {
-	const unreachables: number[] = [];
+function setUnreachableIds(target: Ref<string[]>, elements: HTMLElement[]) {
+	const unreachables: string[] = [];
 	const root = document.documentElement;
 	// This works at any point of the page
 	const scrollStart = root.scrollHeight - root.clientHeight - root.scrollTop;
 
-	// Get all indexes that are unreachable
+	// Get all IDs that are unreachable
 	Array.from(getRects(elements, 'top').values()).forEach((value, index) => {
 		if (value > scrollStart) {
-			unreachables.push(index);
+			unreachables.push(elements[index].id);
 		}
 	});
 
@@ -68,7 +69,7 @@ function _setUnreachables(target: Ref<number[]>, elements: HTMLElement[]) {
 }
 
 export function useHighlight(
-	refs: Ref<HTMLElement[]> | string,
+	userIds: Ref<string[]> | string[],
 	{
 		topOffset: userOffset = 100,
 		debounce = 0,
@@ -76,81 +77,84 @@ export function useHighlight(
 		jumpToLast = true,
 	}: UseHighlightOptions
 ) {
-	// Internal refs
-	const fakeRefs: { value: HTMLElement[] } = { value: [] };
-	const userElements = isRef(refs) ? refs : fakeRefs;
-	const scheduledIndex = ref(-1);
+	// Internal
+	const _userIds = computed<string[]>(() => unref(userIds));
+	const sortedTargets = ref<HTMLElement[]>([]);
+	const sortedIds = computed(() => sortedTargets.value.map(({ id }) => id));
+	const unreachableIds = ref<string[]>([]);
+	const scheduledId = ref('');
 
 	// Returned values
-	const unreachableIndices = ref<number[]>([]);
-	const activeIndex = ref(-1);
-	const activeId = computed(() => userElements.value[activeIndex.value]?.id || '');
+	const activeId = ref('');
 	const dataset = computed<Dataset>(() => {
-		const activeElement = userElements.value[activeIndex.value];
+		const activeElement = sortedTargets.value.find(({ id }) => id === activeId.value);
 		if (!activeElement) {
 			return {};
 		}
 		return getDataset(activeElement.dataset);
 	});
+	const activeIndex = computed(() => sortedIds.value.indexOf(activeId.value));
 
-	function setUnreachable(index: number) {
+	function setUnreachable(id: string) {
 		nextTick(() => {
-			if (unreachableIndices.value.includes(index) && index !== activeIndex.value) {
-				scheduledIndex.value = index;
+			console.log(id);
+			if (unreachableIds.value.includes(id) && id !== activeId.value) {
+				scheduledId.value = id;
 			}
 		});
 	}
 
+	watch(
+		() => _userIds.value,
+		(newIds) => {
+			nextTick(() => {
+				// Get fresh targets
+				sortedTargets.value = [];
+				newIds.forEach((id) => {
+					const target = document.getElementById(id);
+
+					if (target) {
+						sortedTargets.value.push(target);
+					}
+				});
+
+				// Sort targets by DOM order
+				sortedTargets.value.sort((a, b) => a.offsetTop - b.offsetTop);
+
+				// Set fresh unreachable IDs
+				setUnreachableIds(unreachableIds, sortedTargets.value);
+			});
+		},
+		{ immediate: true }
+	);
+
 	onMounted(() => {
-		// Get them here as there's nothing to watch against in order to keep them reactive.
-		if (typeof refs === 'string') {
-			(userElements as Ref<HTMLElement[]>).value = Array.from(document.querySelectorAll(refs));
-		}
+		nextTick(() => {
+			// Sets activeId to first if jumpToFirst is true, even if scrolled for 20px
+			if (jumpToFirst && document.documentElement.scrollTop <= BACK_TO_TOP_OFFSET) {
+				return (activeId.value = sortedTargets.value[0].id);
+			}
 
-		// Sets activeIndex to 0 if jumpToFirst is true, even if scrolled for 20px
-		if (jumpToFirst && document.documentElement.scrollTop <= BACK_TO_TOP_OFFSET) {
-			return (activeIndex.value = 0);
-		}
-
-		// Autmatically set any unreachable index from hash as active
-		const unreachableFromHash = userElements.value.findIndex(
-			({ id }) => id === window.location.hash.slice(1)
-		);
-		setUnreachable(unreachableFromHash);
+			// This properly sets as active any unreachable target on page load, if included in the hash
+			const unreachableIdFromHash = sortedTargets.value.find(
+				({ id }) => id === window.location.hash.slice(1)
+			);
+			setUnreachable(unreachableIdFromHash?.id || '');
+		});
 	});
 
 	watch(
-		[() => userElements.value.length, () => typeof refs],
-		() => {
-			/**
-			 * This runs onMount as well also when user is using selectors.
-			 * We reorder the elements by their offsetTop position as
-			 * it may not be guaranteed according to vue template refs docs.
-			 */
-
-			userElements.value.sort((a, b) => a.offsetTop - b.offsetTop);
-			setUnreachables();
-		},
-		{ immediate: true, flush: 'post' }
-	);
-
-	watch(
-		() => scheduledIndex.value,
+		() => scheduledId.value,
 		(newValue) => {
-			if (newValue !== -1 && isBottomReached.value) {
-				activeIndex.value = newValue;
+			if (newValue !== '' && isBottomReached.value) {
+				activeId.value = newValue;
 			}
 		},
 		{ flush: 'post' }
 	);
 
-	// Internal Handlers
-	function setUnreachables() {
-		_setUnreachables(unreachableIndices, userElements.value);
-	}
-
 	/**
-	 * - getRects(top, -) -> get all titles that are above the viewport, used onScrollDown.
+	 * - getRects(top, -) -> get all titles that are above the viewport, used onScrollDown and onResize.
 	 * Since map order respects DOM order the LAST value is the nearest to the top of the viewport.
 	 *
 	 * - getRects(bottom, +) -> get all titles that entered the viewport, used onScrollUp.
@@ -160,77 +164,84 @@ export function useHighlight(
 
 	function onScrollDown({ isResize } = { isResize: false }) {
 		/**
-		 * This condition prevents to set the first index as active until a title actually
-		 * leaves the viewport. Used when jumpToFirst is false.
+		 * When jumpToFirst is false, this condition prevents to set the first target
+		 * as active until a title actually leaves the viewport.
 		 */
 		if (
 			!jumpToFirst &&
-			activeIndex.value === -1 &&
-			getRects(userElements.value, 'top', '-', userOffset).size <= 0
+			activeId.value === '' &&
+			getRects(sortedTargets.value, 'top', '-', userOffset).size <= 0
 		) {
-			return (activeIndex.value = -1);
+			return (activeId.value = sortedTargets.value[0].id);
 		}
+
 		// Common behavior - Get last item that leaves the viewport from its top edge
-		const newActiveIndex =
-			Array.from(getRects(userElements.value, 'top', '-', userOffset).keys()).pop() ?? 0;
+		const newActiveId =
+			Array.from(getRects(sortedTargets.value, 'top', '-', userOffset).keys()).pop() ?? '';
 
 		if (!isResize) {
 			/**
-			 * This condition prevents to set PREV indexes as active for when scrolling down
+			 * This condition prevents to set PREV targets as active for when scrolling down
 			 * with smoothscroll is active.
 			 */
-			if (newActiveIndex > activeIndex.value) {
-				activeIndex.value = newActiveIndex;
+			if (sortedIds.value.indexOf(newActiveId) > sortedIds.value.indexOf(activeId.value)) {
+				activeId.value = newActiveId;
 			}
 		} else {
-			activeIndex.value = newActiveIndex;
+			activeId.value = newActiveId;
 		}
 	}
 
 	function onScrollUp() {
-		// Reset any scheduled index
-		scheduledIndex.value = -1;
+		// Reset any unreachable scheduled id
+		scheduledId.value = '';
+
 		// Common behavior - Get first item that enters the viewport from its bottom edge
-		const newActiveIndex =
-			getRects(userElements.value, 'bottom', '+', userOffset).keys().next().value ?? 0;
+		const newActiveId =
+			getRects(sortedTargets.value, 'bottom', '+', userOffset).keys().next().value ??
+			sortedIds.value[0];
 
 		/**
 		 * If jumpToFirst is false, and the first title is in the viewport,
 		 * we set activeIndex to -1 as soon as it is completely in the viewport (top edge positive).
 		 */
-		if (!jumpToFirst && newActiveIndex === 0) {
-			const newActiveTopPos = getRects(userElements.value, 'top').values().next().value ?? 0;
+		if (!jumpToFirst && newActiveId === sortedIds.value[0]) {
+			const newActiveTopPos = getRects(sortedTargets.value, 'top').values().next().value ?? 0;
 			if (newActiveTopPos > BACK_TO_TOP_OFFSET + userOffset) {
-				return (activeIndex.value = -1);
+				return (activeId.value = '');
 			}
 		}
 
 		/**
-		 * This condition prevents to set NEXT indexes as active when scrolling up
+		 * This condition prevents to set NEXT targets as active when scrolling up
 		 * with smoothscroll is active.
 		 */
-		if (newActiveIndex < activeIndex.value) {
-			activeIndex.value = newActiveIndex;
+		if (sortedIds.value.indexOf(newActiveId) < sortedIds.value.indexOf(activeId.value)) {
+			activeId.value = newActiveId;
 		}
 	}
 
 	function onBottomReached() {
-		// If jumpToLast is true and no scheduled index is set, set the last unreachabe index one as active.
-		if (jumpToLast && unreachableIndices.value.length > 0 && scheduledIndex.value === -1)
-			return (activeIndex.value = unreachableIndices.value[unreachableIndices.value.length - 1]);
+		// If jumpToLast is true and no scheduled unreachable ID is set, set the last unreachabe ID as active.
+		if (jumpToLast && unreachableIds.value.length > 0 && scheduledId.value === '') {
+			return (activeId.value = unreachableIds.value[unreachableIds.value.length - 1]);
+		}
 
 		/**
-		 * If there's a scheduled index from outside, set it as active.
+		 * If there's a scheduled unreachable ID from outside, set it as active.
 		 * This occurse whenever user triggers setUnreachable.
 		 */
-		if (scheduledIndex.value !== -1) {
-			activeIndex.value = scheduledIndex.value;
+		if (scheduledId.value !== '') {
+			activeId.value = scheduledId.value;
 		}
 	}
 
-	useResize(setUnreachables, () => onScrollDown({ isResize: true }));
+	useResize(
+		() => setUnreachableIds(unreachableIds, sortedTargets.value),
+		() => onScrollDown({ isResize: true })
+	);
 
-	const { isBottomReached } = useScroll(userElements, {
+	const { isBottomReached } = useScroll(sortedTargets, {
 		onScrollDown,
 		onScrollUp,
 		onBottomReached,
@@ -241,7 +252,7 @@ export function useHighlight(
 		activeIndex,
 		activeId,
 		dataset,
-		unreachableIndices,
+		unreachableIds,
 		isBottomReached,
 		setUnreachable,
 	};
