a474834b4fe88dfd0605a1ad0a561c009cbf11fa
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2023-01-03 20:45:58 +0100

    proper scroll linstener refresh, get rid of dataset

--- src/useScroll.ts
+++ src/useScroll.ts
@@ -1,77 +1,106 @@
-import { watch, ComputedRef, onMounted, ref } from 'vue';
+import { watch, onMounted, ref, onBeforeMount } from 'vue';
 import { IDLE_TIME, isSSR } from './utils';
 
 type UseScrollOptions = {
 	onScroll: (prevPos: number) => void;
-	isAbove: ComputedRef<boolean>;
+	minWidth: number;
 };
 
-export function useScroll({ onScroll, isAbove }: UseScrollOptions) {
+export function useScroll({ onScroll: _onScroll, minWidth }: UseScrollOptions) {
 	const isClick = ref(false);
 
 	if (isSSR) {
 		return isClick;
 	}
 
-	let isIdle = false;
+	const media = `(min-width: ${minWidth}px)`;
+	const matchMedia = ref(window.matchMedia(media).matches);
+	const isIdle = ref(false);
+	const restartCount = ref(0);
+
 	let idleTimer: NodeJS.Timeout;
-	let clickTimer: NodeJS.Timeout;
 	let prevY: number | undefined;
+
+	function onResize() {
+		matchMedia.value = window.matchMedia(media).matches;
+	}
+
+	function onScroll() {
+		// Do not update results if scrolling from click or on mount
+		if (isIdle.value && !isClick.value) {
+			if (!prevY) {
+				prevY = window.scrollY;
+			}
+			_onScroll(prevY);
+			prevY = window.scrollY;
+		}
+	}
 
 	function onIdle() {
 		clearTimeout(idleTimer);
 		idleTimer = setTimeout(() => {
 			console.log('Idle');
-			isIdle = true;
+			isIdle.value = true;
 		}, IDLE_TIME);
 	}
 
-	function onClickIdle() {
-		clickTimer = setTimeout(() => {
-			console.log('Reset click timer');
-			isClick.value = false;
-		}, IDLE_TIME);
-	}
-
-	function _onScroll() {
-		if (isIdle) {
-			clearTimeout(clickTimer);
-
-			if (!isClick.value) {
-				if (!prevY) {
-					prevY = window.scrollY;
-				}
-				onScroll(prevY);
-				prevY = window.scrollY;
-			}
-
-			onClickIdle();
-		}
+	// Restart the main scroll listener if attempting to scroll while scrolling from click
+	function onRestart() {
+		isClick.value = false;
+		restartCount.value++;
 	}
 
 	onMounted(() => {
+		window.addEventListener('resize', onResize, { passive: true });
 		if (window.scrollY > 0) {
 			document.addEventListener('scroll', onIdle, { once: true, passive: true });
 		} else {
-			isIdle = true;
+			isIdle.value = true;
 		}
 	});
 
 	watch(
-		isAbove,
-		(_isAbove, _, onCleanup) => {
-			if (_isAbove) {
-				console.log('Attaching scroll...');
-				document.addEventListener('scroll', _onScroll, { passive: true });
+		[isIdle, matchMedia, restartCount],
+		([_isIdle, _matchMedia], [], onCleanup) => {
+			if (_isIdle) {
+				if (_matchMedia) {
+					console.log('Attaching scroll...');
+					document.addEventListener('scroll', onScroll, {
+						passive: true,
+					});
+				}
+
+				onCleanup(() => {
+					console.log('Removing scroll...');
+					document.removeEventListener('scroll', onScroll);
+				});
+			}
+		},
+		{ immediate: true, flush: 'sync' }
+	);
+
+	watch(
+		isClick,
+		(hasClicked, _, onCleanup) => {
+			if (hasClicked) {
+				if ('ontouchstart' in window) {
+					document.addEventListener('touchstart', onRestart, { once: true });
+				} else {
+					document.addEventListener('wheel', onRestart, { once: true });
+				}
 			}
 
 			onCleanup(() => {
-				console.log('Removing scroll...');
-				document.removeEventListener('scroll', _onScroll);
+				document.removeEventListener('touchstart', onRestart);
+				document.removeEventListener('wheel', onRestart);
 			});
 		},
-		{ immediate: true }
+		{ flush: 'sync' }
 	);
+
+	onBeforeMount(() => {
+		window.removeEventListener('resize', onResize);
+	});
 
 	return isClick;
 }
